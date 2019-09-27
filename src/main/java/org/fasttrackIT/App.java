b6d303dddc73c1723f182ce76f6e6338769570e6
Roland <rolandattila@yahoo.com>
2019-09-27 18:26:53 +0300

    added overloading methods

--- src/main/java/org/fasttrackIT/App.java
+++ src/main/java/org/fasttrackIT/App.java
@@ -9,5 +9,10 @@ public class App {
         simpleMath.divide(simpleMath, simpleMath);
         simpleMath.multiply(simpleMath, simpleMath);
         simpleMath.modulus(simpleMath, simpleMath);
+        simpleMath.minus(244.5, 122.2);
+        simpleMath.plus(244.2, 222.5);
+        simpleMath.divide(90.9, 30.3);
+        simpleMath.multiply(320.8, 320.9);
+        simpleMath.modulus(780.99, 81.23);
     }
 }
--- src/main/java/org/fasttrackIT/SimpleMath.java
+++ src/main/java/org/fasttrackIT/SimpleMath.java
@@ -5,6 +5,7 @@ public class SimpleMath {
     int a;
     int b;
     int c;
+    double cc;
 
     public int minus(SimpleMath one, SimpleMath two) {
         one.a = 110;
@@ -12,6 +13,14 @@ public class SimpleMath {
         c = one.a - two.b;
         System.out.println("Result: " + c);
         return (c);
+    }
+
+    public double minus(double one, double two) {
+        one = 244.5;
+        two = 122.2;
+        cc = one - two;
+        System.out.println("Result: " + cc);
+        return (cc);
     }
 
     public int plus(SimpleMath one, SimpleMath two) {
@@ -22,12 +31,28 @@ public class SimpleMath {
         return (c);
     }
 
+    public double plus(double one, double two) {
+        one = 244.2;
+        two = 222.5;
+        cc = one + two;
+        System.out.println("Result: " + cc);
+        return (cc);
+    }
+
     public int divide(SimpleMath one, SimpleMath two) {
         one.a = 90;
         two.b = 30;
         c = one.a / two.b;
         System.out.println("Result: " + c);
         return (c);
+    }
+
+    public double divide(double one, double two) {
+        one = 90.9;
+        two = 30.3;
+        cc = one / two;
+        System.out.println("Result: " + cc);
+        return (cc);
     }
 
     public int multiply(SimpleMath one, SimpleMath two) {
@@ -38,12 +63,28 @@ public class SimpleMath {
         return (c);
     }
 
+    public double multiply(double one, double two) {
+        one = 320.8;
+        two = 320.9;
+        cc = one * two;
+        System.out.println("Result: " + cc);
+        return (cc);
+    }
+
     public int modulus(SimpleMath one, SimpleMath two) {
         one.a = 780;
         two.b = 81;
         c = one.a % two.b;
         System.out.println("Result: " + c);
         return (c);
+    }
+
+    public double modulus(double one, double two) {
+        one = 780.99;
+        two = 81.23;
+        cc = one % two;
+        System.out.println("Result: " + cc);
+        return (cc);
     }
 
 }
